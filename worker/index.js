7ee04627b1010969c56bbb71cfa5696e9938c7aa
Christian Schaefer <schaepp@gmx.de>
2021-01-25 06:49:40 +0100

    Simplified worker

--- worker/index.js
+++ worker/index.js
@@ -1,30 +1,10 @@
 
 async function handleRequest(request) {
-  const host = 'www.duesseldorf.de';
   const url = new URL(request.url);
-  let originUrl = url.protocol + '//' + host + url.pathname + url.search;
-  let cacheTtl;
-
-  switch (true) {
-    default:
-      break;
-
-    case request.url.includes('/fileadmin/_processed_/'):
-    case request.url.includes('.js?'):
-    case request.url.includes('.woff2'):
-    case request.url.includes('.woff'):
-      cacheTtl = 31536000;
-      break;
-  }
-
+  const originUrl = url.protocol + '//www.duesseldorf.de' + url.pathname + url.search;
   const proxiedRequest = new Request(originUrl, request);
-  return await fetch(proxiedRequest, {
-    cf: {
-      cacheTtl,
-      cacheEverything: true,
-      cacheKey: request.url
-    },
-  });
+
+  return await fetch(proxiedRequest);
 }
 
 addEventListener('fetch', event => {
